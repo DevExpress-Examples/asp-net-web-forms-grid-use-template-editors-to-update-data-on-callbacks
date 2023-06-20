d0b6bd0c40a2d1f4fd49f6d5c27a8be349a1911a
DevExpressExampleBot <fakeEmail>
2023-06-20 22:39:22 +0400

    Source auto update [skip ci]

---
+++ VB/WebSite/App_Code/XpoHelper.vb
@@ -0,0 +1,74 @@
+﻿Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Xpo
+Imports DevExpress.Xpo.DB
+Imports DevExpress.Xpo.Metadata
+
+''' <summary>
+''' Summary description for XpoHelper
+''' </summary>
+Public Module XpoHelper
+	Sub New()
+		CreateDefaultObjects()
+	End Sub
+
+	Public Function GetNewSession() As Session
+		Return New Session(DataLayer)
+	End Function
+
+	Public Function GetNewUnitOfWork() As UnitOfWork
+		Return New UnitOfWork(DataLayer)
+	End Function
+
+	Private ReadOnly lockObject As New Object()
+
+	Private fDataLayer As IDataLayer
+	Private ReadOnly Property DataLayer() As IDataLayer
+		Get
+			If fDataLayer Is Nothing Then
+				SyncLock lockObject
+					fDataLayer = GetDataLayer()
+				End SyncLock
+			End If
+			Return fDataLayer
+		End Get
+	End Property
+
+	Private Function GetDataLayer() As IDataLayer
+		XpoDefault.Session = Nothing
+
+		Dim ds As New InMemoryDataStore()
+		Dim dict As XPDictionary = New ReflectionDictionary()
+		dict.GetDataStoreSchema(GetType(MyObject).Assembly)
+
+		Return New ThreadSafeDataLayer(dict, ds)
+	End Function
+
+	Private Sub CreateDefaultObjects()
+		Using uow As UnitOfWork = GetNewUnitOfWork()
+			Dim obj As New MyObject(uow)
+			obj.Title = "Item 1"
+			obj.Item = 0
+			obj.SomeDate = DateTime.Now
+
+			obj = New MyObject(uow)
+			obj.Title = "Item 2"
+			obj.Item = 1
+			obj.SomeDate = DateTime.Now.AddMonths(1)
+
+			obj = New MyObject(uow)
+			obj.Title = "Item 3"
+			obj.Item = 2
+			obj.SomeDate = DateTime.Now.AddMonths(2)
+
+			uow.CommitChanges()
+		End Using
+	End Sub
+End Module
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,61 @@
+﻿Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Xpo
+Imports DevExpress.Web
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Private session As Session = XpoHelper.GetNewSession()
+
+	Private Const CallbackArgumentFormat As String = "function (s, e) {{ OnValueChanged(s,e, {0}, '{1}'); }}"
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		xds.Session = session
+	End Sub
+
+	Protected Sub editor_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim txt As ASPxEdit = TryCast(sender, ASPxTextEdit)
+		Dim container As GridViewDataItemTemplateContainer = TryCast(txt.NamingContainer, GridViewDataItemTemplateContainer)
+
+		txt.SetClientSideEventHandler("ValueChanged", String.Format(CallbackArgumentFormat, container.KeyValue, container.Column.FieldName))
+	End Sub
+
+	Protected Sub cb_Callback(ByVal source As Object, ByVal e As DevExpress.Web.CallbackEventArgs)
+		Dim p() As String = e.Parameter.Split("|"c)
+
+		Dim key As Int32 = Convert.ToInt32(p(0))
+		Dim field As String = p(1)
+		Dim value As Object = p(2)
+
+		Dim obj As MyObject = session.GetObjectByKey(Of MyObject)(key)
+
+		Select Case field
+			Case "Title"
+				If value.ToString() = "null" Then
+					value = String.Empty
+				End If
+
+			Case "Item"
+				value = Convert.ToInt32(value)
+
+			Case "SomeDate"
+				Dim result As DateTime = DateTime.Now
+				If DateTime.TryParse(value.ToString(), result) Then
+					value = result
+				Else
+					value = DateTime.Now
+				End If
+		End Select
+
+		obj.SetMemberValue(field, value)
+
+		obj.Save()
+	End Sub
+End Class
